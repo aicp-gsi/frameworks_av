fcb0de132e8da173cf5fbe9c0892d8cae3e6b0fa
aicp-gsi <52255412+aicp-gsi@users.noreply.github.com>
2019-06-28 04:49:58 +0400

    Fix bad cherry pick

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -298,17 +298,10 @@ void CameraService::onDeviceStatusChanged(const String8& id,
             ALOGI("%s: Unknown camera ID %s, a new camera is added",
                     __FUNCTION__, id.string());
 
-<<<<<<< HEAD
-#ifndef CAMERA_NEEDS_ADD_STATES_IN_ENUMERATE
-            // First add as absent to make sure clients are notified below
-            addStates(id);
-#endif
-=======
    if (!property_get_bool("persist.sys.camera.huawei", false)) {
             // First add as absent to make sure clients are notified below
             addStates(id);
    }
->>>>>>> 6b481b7... CameraService: Support calling addStates in enumerateProviders
 
             updateStatus(newStatus, id);
         } else {
